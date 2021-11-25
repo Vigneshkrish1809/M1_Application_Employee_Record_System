b22ec657f3f072a5534c07370783949555b1aa63
Vigneshkrish1809 <70203719+Vigneshkrish1809@users.noreply.github.com>
2021-11-25 14:07:34 +0530

    Update feature.h

--- 3_Implementations/inc/feature.h
+++ 3_Implementations/inc/feature.h
@@ -1,1 +1,15 @@
+#ifndef FEATURE_H_
+#define FEATURE_H_
 
+
+#include <stdio.h> ///for input output functions like printf, scanf
+#include <stdlib.h>
+#include <windows.h> ///for windows related functions (not important)
+#include <string.h>  ///string operations
+
+char name[40]; ///name of employee
+        int age; /// age of employee
+        float bs; /// basic salary of employee
+
+
+#endif
