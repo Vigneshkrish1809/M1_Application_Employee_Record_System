c2626b92c65efc0f35c17fd795b955546bf6f4dd
Vigneshkrish1809 <70203719+Vigneshkrish1809@users.noreply.github.com>
2021-11-25 14:15:02 +0530

    Update main.c

--- 3_Implementations/main.c
+++ 3_Implementations/main.c
@@ -1,1 +1,16 @@
+#include "header.h"
 
+/** List of Global Variable */
+COORD coord = {0,0}; /// top-left corner of window
+
+/**
+    function : gotoxy
+    @param input: x and y coordinates
+    @param output: moves the cursor in specified position of console
+*/
+void gotoxy(int x,int y)
+{
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
